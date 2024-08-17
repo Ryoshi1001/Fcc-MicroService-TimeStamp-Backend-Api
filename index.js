6d3b611ea1735cfd834f8cfc5aabd68cf9b0d96e
ryoshi1001 <ryoshi16421@proton.me>
2024-08-17 01:05:31 -0700

    adding repo to github

--- index.js
+++ index.js
@@ -18,11 +18,45 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+
+
+
+// Me API endpoint made for: Timestamp Microservice Freecodecamp Express/API/JS
+app.get("/api/:date?", function(req, res){
+  //capture input from url
+  const inputRouteParam = req.params.date; 
+
+  //start initialize date variable
+  let date; 
+
+  // check if input is empty or not
+  if(inputRouteParam === '' || inputRouteParam === undefined){
+    date = new Date() 
+  } else if(!isNaN(inputRouteParam)){
+    date = new Date(Number(inputRouteParam))
+  } else{
+    date = new Date(inputRouteParam)
+  }
+
+  //validate the date
+  if(isNaN(date.getTime())){
+    return res.json({ error : "Invalid Date"})
+  }
+
+  //variable for unix time and utc date
+  const time = date.getTime(); 
+  const dateUTC = date.toUTCString(); 
+
+  //res.json to client with format needed
+  res.json({ unix: time, utc: dateUTC})
+})
+
+
 
 
 
